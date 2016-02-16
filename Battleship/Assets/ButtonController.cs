1ec1c72c416381b3eaa0f60b8216c96745443b39
Jay Byam <jrbyam@Brock.local>
2016-02-15 15:18:47 -0900

    Added basic main scene

---
+++ Battleship/Assets/ButtonController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ButtonController : MonoBehaviour {
+
+	public void nextScene(string nextScene) {
+		SceneManager.LoadScene (nextScene);
+	}
+}
