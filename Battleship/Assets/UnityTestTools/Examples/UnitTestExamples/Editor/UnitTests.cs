1f7069e45d61a01167a134cb804c0eb90ce87cd6
Jay Byam <jrbyam@Brock.local>
2016-02-22 15:25:21 -0900

    Did a bit on the main menu and started a tests class

---
+++ Battleship/Assets/UnityTestTools/Examples/UnitTestExamples/Editor/UnitTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using NUnit.Framework;
+using NSubstitute;
+using UnityEngine;
+
+namespace UnityTest {
+	[TestFixture]
+	[Category ("Unit Tests")]
+	internal class UnitTests {
+
+		[Test]
+		public void testIfSunk () {
+			Ship ship = Substitute.For<Ship>(5);
+
+			for (int i = 0; i < ship.size; i++) {
+				GridSquare square = new GridSquare (1, i);
+				square.hit = true;
+				ship.gridSquares.Add (square);
+			}
+			if (ship.sunk) {
+				Assert.Pass ();
+			}
+		}
+	}
+}
