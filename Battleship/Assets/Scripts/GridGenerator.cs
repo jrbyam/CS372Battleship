d29460cff66548f72c8036d28c13eeca1792f2f6
Jay Byam <jrbyam@Brock.local>
2016-02-26 00:36:29 -0900

    Added grid with highlighting when touched

---
+++ Battleship/Assets/Scripts/GridGenerator.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridGenerator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		assignGridSquares ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void assignGridSquares () {
+		for (int i = 1; i <= 10; ++i) {
+			for (int j = 1; j <= 10; ++j) {
+				GridSquare square = GameObject.Find (i + " " + j).AddComponent<GridSquare> ();
+				square.row = i;
+				square.column = j;
+				// Position each square above the water
+				square.gameObject.transform.position = new Vector3 (square.gameObject.transform.position.x, 1, square.gameObject.transform.position.z);
+			}
+		}
+	}
+}
--- Battleship/Assets/Scripts/GridSquare.cs
+++ Battleship/Assets/Scripts/GridSquare.cs
@@ -1,12 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GridSquare {
+public class GridSquare : MonoBehaviour {
 
 	public int row;
 	public int column;
 	public bool hit = false;
 	public bool occupied;
+	private Plane plane = new Plane(Vector3.up, -1);
+	private float distance;
 
 	public GridSquare() {
 		row = 0;
@@ -17,6 +19,21 @@ public class GridSquare {
 	public GridSquare(int row, int column) {
 		this.row = row;
 		this.column = column;
+	}
+
+	void Update () {
+		if (Input.GetMouseButton (0)) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			if (plane.Raycast (ray, out distance)) {
+				if (GetComponent<BoxCollider> ().bounds.Contains (ray.GetPoint (distance))) { // Press is over this square
+					GetComponent<Renderer> ().material.color = new Vector4 (1, 1, 1, 0.7f);
+				} else {
+					GetComponent<Renderer> ().material.color = new Vector4 (1, 1, 1, 0.16f); // Press is over a different square
+				}
+			}
+		} else {
+			GetComponent<Renderer> ().material.color = new Vector4 (1, 1, 1, 0.16f);
+		}
 	}
 
 	public bool isPlacedLegally () {
